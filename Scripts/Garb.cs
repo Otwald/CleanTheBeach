e15977a85b0bc97807c4a3ffbdb68b3da17b0f13
otwald <hinterlanddev@googlemail.com>
2020-06-06 10:44:09 +0200

    updated player collision shape and added gravity to garbage object

--- Scripts/Garb.cs
+++ Scripts/Garb.cs
@@ -3,19 +3,18 @@ using System;
 
 public class Garb : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
+    [Export] public int gravity = 1200;
+    private Vector2 velocity = new Vector2();
     // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        
-    }
+    // public override void _Ready()
+    // {
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    // }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        velocity.y += gravity * delta;
+        velocity = MoveAndSlide(velocity, new Vector2(0, -1));
+    }
 }
